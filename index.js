0b5bdd40f91d234a8a47eae5827b1e015a892d4f
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 17:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class Driver{
     store.drivers.push(this);
   };
 
-  trips(){
-    return store.trips.filter( trip => {return trip.userID === this.id})
-  }
+  // trips(){
+  //   return store.trips.filter( trip => {return trip.userID === this.id})
+  // }
 };
 
 // class Passengers{
